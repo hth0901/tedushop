89ce0677169186a744594f3875ed0b630dac7169
kuxidaica <hth0901@gmail.com>
2018-08-06 12:40:07 +0700

    ~ thêm filter

--- TeduShop.Web/app/components/product_categories/productCategoryListController.js
+++ TeduShop.Web/app/components/product_categories/productCategoryListController.js
@@ -1,7 +1,22 @@
 ﻿/// <reference path="D:\learning\TeduShop\TeduShop.Web\Assets/admin/libs/angular/angular.js" />
 (function (app) {
     app.controller('productCategoryListController', productCategoryListController);
-    function productCategoryListController() {
 
+    productCategoryListController.$inject = ['$scope', 'apiService'];
+
+    function productCategoryListController($scope, apiService) {
+        $scope.productCategories = [];
+
+        $scope.getProductCategories = getProductCategories;
+
+        function getProductCategories() {
+            apiService.get('/api/productcategory/getall', null, function (result) {
+                $scope.productCategories = result.data;
+            }, function () {
+                console.log('Load productcategory failed!!!');
+            });
+        };
+
+        $scope.getProductCategories();
     };
 })(angular.module('tedushop.product_categories'));
---
+++ TeduShop.Web/app/shared/filters/statusFilter.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="D:\learning\TeduShop\TeduShop.Web\Assets/admin/libs/angular/angular.js" />
+(function (app) {
+    app.filter('statusFilter', function () {
+        return function (input) {
+            if (input == true)
+                return 'Kich hoat';
+            return 'Khoa'
+        }
+    });
+})(angular.module('tedushop.common'));
