bb11f63ccbe36ab94174dcdc2b3bb884f8f7e7d7
kuxidaica <hth0901@gmail.com>
2018-08-16 13:50:40 +0700

    + them binding data for category menu

--- TeduShop.Web/Controllers/HomeController.cs
+++ TeduShop.Web/Controllers/HomeController.cs
@@ -1,13 +1,24 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TeduShop.Model.Models;
+using TeduShop.Service;
+using TeduShop.Web.Models;
 
 namespace TeduShop.Web.Controllers
 {
     public class HomeController : Controller
     {
+        IProductCategoryService _productCategoryService;
+
+        public HomeController(IProductCategoryService productCategoryService)
+        {
+            this._productCategoryService = productCategoryService;
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -41,7 +52,9 @@ namespace TeduShop.Web.Controllers
         [ChildActionOnly]
         public ActionResult Category()
         {
-            return PartialView("Category");
+            var model = _productCategoryService.GetAll();
+            var lstProductCategoryViewModel = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
+            return PartialView("Category", lstProductCategoryViewModel);
         }
     }
 }
