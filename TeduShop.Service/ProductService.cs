5c4a85a0f20c93b63ac35ae6fe077741119a5aba
kuxidaica <hth0901@gmail.com>
2018-08-18 21:50:23 +0700

    ~ finish lesson 45: sap xep danh sach san pham

--- TeduShop.Service/ProductService.cs
+++ TeduShop.Service/ProductService.cs
@@ -20,7 +20,7 @@ namespace TeduShop.Service
         IEnumerable<Product> GetAllByParentId(int parentId);
         IEnumerable<Product> GetLastest(int top);
         IEnumerable<Product> GetHotProducts(int top);
-        IEnumerable<Product> GetListProductByCategoryIdPaging(int categoryId, int page, int pageSize, out int totalRow);
+        IEnumerable<Product> GetListProductByCategoryIdPaging(int categoryId, int page, int pageSize, string sort, out int totalRow);
 
         Product GetById(int id);
         void Save();
@@ -140,9 +140,29 @@ namespace TeduShop.Service
             return this._productRepository.GetMulti(x => x.Status && x.HotFlag == true).OrderByDescending(x => x.CreatedDate).Take(top);
         }
 
-        public IEnumerable<Product> GetListProductByCategoryIdPaging(int categoryId, int page, int pageSize, out int totalRow)
+        public IEnumerable<Product> GetListProductByCategoryIdPaging(int categoryId, int page, int pageSize, string sort, out int totalRow)
         {
             var query = this._productRepository.GetMulti(x => x.Status && x.CategoryID == categoryId);
+
+            switch (sort)
+            {
+                case "new":
+                    query = query.OrderByDescending(x => x.CreatedDate);
+                    break;
+                case "popular":
+                    query = query.OrderByDescending(x => x.ViewCount);
+                    break;
+                case "discount":
+                    query = query.OrderByDescending(x => x.PromotionPrice.HasValue);
+                    break;
+                case "price":
+                    query = query.OrderBy(x => x.Price);
+                    break;
+                default:
+                    query = query.OrderByDescending(x => x.CreatedDate);
+                    break;
+            }
+
             totalRow = query.Count();
             return query.Skip((page - 1) * pageSize).Take(pageSize);
         }
--- TeduShop.Web/Controllers/ProductController.cs
+++ TeduShop.Web/Controllers/ProductController.cs
@@ -27,11 +27,11 @@ namespace TeduShop.Web.Controllers
             return View();
         }
 
-        public ActionResult Category(int id, int page = 1)
+        public ActionResult Category(int id, int page = 1, string sort = "")
         {
             int pageSize = int.Parse(ConfigHelper.GetByKey("PageSize"));
             int totalRow = 0;
-            var lstProductModel = this._productService.GetListProductByCategoryIdPaging(id, page, pageSize, out totalRow);
+            var lstProductModel = this._productService.GetListProductByCategoryIdPaging(id, page, pageSize, sort, out totalRow);
 
             var lstProductViewModel = Mapper.Map<IEnumerable<Product>, IEnumerable<ProductViewModel>>(lstProductModel);
 
